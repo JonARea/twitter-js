5a3a29a4dbf0c5cea15d7de919e672e3261625ea
Jon Rea <jonarea@gmail.com>
2017-11-06 21:10:52 -0500

    nunjucks setup complete

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const chalk = require('chalk')
+const nunjucks = require('nunjucks')
+
+app.set('view engine', 'html')
+//app.engine('html', nunjucks.render)
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true,
+  noCache: true
+});
 
 app.use((req, res, next) => {
   console.log(chalk.red(req.method, req.originalUrl))
@@ -12,7 +23,7 @@ app.use('/special', (req,res,next) => {
 })
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Twitter App')
+  res.render('index', { title: 'Twitter-JS', people: [{name: 'Gandalf'}, {name: 'Hermione'}, {name: 'Frodo'}]})
 })
 
 app.listen(3000, () => {
